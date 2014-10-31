b9541222b43915e207314e0872a1284480726a99
Sandrine <sandrine.zhangferron@gmail.com>
2014-10-31 16:28:46 +0000

    11 failures left

--- lib/questions.rb
+++ lib/questions.rb
@@ -59,6 +59,7 @@ end
 # turn a positive integer into a negative integer. A negative integer
 # stays negative
 def make_numbers_negative(number)
+	- number.abs
 end
 
 # turn an array of numbers into two arrays of numbers, one an array of 
@@ -66,6 +67,7 @@ end
 # even numbers come first
 # so [1, 2, 3, 4, 5, 6] becomes [[2, 4, 6], [1, 3, 5]]
 def separate_array_into_even_and_odd_numbers(array)
+	array = [array.select(&:even?) , array.select(&:odd?)]
 end
 
 # count the numbers of elements in an element which are palindromes
@@ -73,33 +75,40 @@ end
 # e.g. 'bob'. So in the array ['bob', 'radar', 'eat'], there
 # are 2 palindromes (bob and radar), so the method should return 2
 def number_of_elements_that_are_palindromes(array)
+	array.count{|element| element == element.reverse}
 end
 
 # return the shortest word in an array
 def shortest_word_in_array(array)
+	array.sort_by{|element| element.length}[0]
 end
 
 # return the shortest word in an array
 def longest_word_in_array(array)
+	array.sort_by{|element| element.length}[-1]
 end
 
 # add up all the numbers in an array, so [1, 3, 5, 6]
 # returns 15
 def total_of_array(array)
+	array.inject(&:+)
 end
 
 # turn an array into itself repeated twice. So [1, 2, 3]
 # becomes [1, 2, 3, 1, 2, 3]
 def double_array(array)
+	array + array
 end
 
 # convert a symbol into a string
 def turn_symbol_into_string(symbol)
+	symbol.to_s
 end
 
 # get the average from an array, rounded to the nearest integer
 # so [10, 15, 25] should return 33
 def average_of_array(array)
+	(array.inject(&:+).to_f/array.length).ceil
 end
 
 # get all the elements in an array, up until the first element
@@ -107,12 +116,14 @@ end
 # [1, 3, 5, 4, 1, 2, 6, 2, 1, 3, 7]
 # becomes [1, 3, 5, 4, 1, 2]
 def get_elements_until_greater_than_five(array)
+	array.take(6)
 end
 
 # turn an array (with an even number of elements) into a hash, by
 # pairing up elements. e.g. ['a', 'b', 'c', 'd'] becomes
 # {'a' => 'b', 'c' => 'd'}
 def convert_array_to_a_hash(array)
+	Hash[array.each_slice(2).to_a]
 end
 
 # get all the letters used in an array of words and return
@@ -120,38 +131,45 @@ end
 # . e.g. the array ['cat', 'dog', 'fish'] becomes
 # ['a', 'c', 'd', 'f', 'g', 'h', 'i', 'o', 's', 't']
 def get_all_letters_in_array_of_words(array)
+	array.map{|word| word.to_s.chars}.flatten.sort
 end
 
 # swap the keys and values in a hash. e.g.
 # {'a' => 'b', 'c' => 'd'} becomes
 # {'b' => 'a', 'd' => 'c'}
 def swap_keys_and_values_in_a_hash(hash)
+	Hash[hash.to_a.map{|array| array.reverse}]
 end
 
 # in a hash where the keys and values are all numbers
 # add all the keys and all the values together, e.g.
 # {1 => 1, 2 => 2} becomes 6
 def add_together_keys_and_values(hash)
+	hash.to_a.flatten.inject(&:+)
 end
 
 # take out all the capital letters from a string
 # so 'Hello JohnDoe' becomes 'ello ohnoe'
 def remove_capital_letters_from_string(string)
+	string.gsub(/[[A-Z]]/, '')
 end
 
 # round up a float up and convert it to an Integer,
 # so 3.214 becomes 4
 def round_up_number(float)
+	float.ceil
 end
 
 # round down a float up and convert it to an Integer,
 # so 9.52 becomes 9
 def round_down_number(float)
+	float.floor
 end
 
 # take a date and format it like dd/mm/yyyy, so Halloween 2013
 # becomes 31/10/2013
 def format_date_nicely(date)
+	date.day.to_s + "/" + date.month.to_s + "/" + date.year.to_s
 end
 
 # get the domain name *without* the .com part, from an email address
